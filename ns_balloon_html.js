c68f12b16b38d4fce285a099f60807640d5ff3da
wekaone <47196099+wekaone@users.noreply.github.com>
2021-07-09 16:04:58 +0300

    Update ns_balloon_html.js

--- ns_balloon_html.js
+++ ns_balloon_html.js
@@ -1483,7 +1483,7 @@ function init() {
         .add(new ymaps.Placemark([53.30704688152192,50.28914832598631], {
             // Зададим содержимое заголовка балуна.
             balloonContentHeader: '<a href = "#popup:stepform" target="_blank">Офис отдела продаж и аренды<br>ЖК "Новая Самара"</a><br>' +
-            '<span class="description">ООО "СЗ "ФИНСТРОЙ"<br><br><i>Режим работы:</i><br>пн-пт с 09 до 19<br>сб с 09 до 16<br>вс - выходной<br><i>Адрес:</i><br><a href="https://yandex.ru/maps/51/samara/?ll=50.289406%2C53.307253&mode=poi&poi%5Bpoint%5D=50.289373%2C53.307363&poi%5Buri%5D=ymapsbm1%3A%2F%2Forg%3Foid%3D2999599358&z=21">г. Самара, Красноглинский р-н, <br>поселок Мехзавод 1- й квартал, д. 40</a></span>',
+            '<span class="description">ООО "СЗ "ФИНСТРОЙ"<br><br><i>Режим работы:</i><br>пн-пт с 09 до 19<br>сб с 09 до 16<br>вс - выходной<br><br><i>Адрес:</i><br><a href="https://yandex.ru/maps/51/samara/?ll=50.289406%2C53.307253&mode=poi&poi%5Bpoint%5D=50.289373%2C53.307363&poi%5Buri%5D=ymapsbm1%3A%2F%2Forg%3Foid%3D2999599358&z=21">г. Самара, Красноглинский р-н, <br>поселок Мехзавод 1- й квартал, д. 40</a></span>',
             // Зададим содержимое основной части балуна.
             balloonContentBody: '<img src="https://static.tildacdn.com/tild3131-3535-4731-b633-653238653735/office.jpg" height="150" width="300" title="Новая Самара" alt="ЖК Новая Самара"> <br/> ' +           
             '<b><a href = "#popup:zvonok" target="_blank" style="font-size: 14px">➥ <u>Заказать обратный звонок</u></a></b>',
